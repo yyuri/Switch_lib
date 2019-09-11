1f80f9119c69e65045a93f23c3da97212b360455
yyuri <oriol.goma@gmail.com>
2019-09-11 10:57:36 +0200

    Add files via upload

---
+++ examples/Period/Period.ino
@@ -0,0 +1,26 @@
+/*
+ This code will turn ON and OFF a LED attached to pin 13. 
+ It will stay ON for 5 seconds
+ It will stay OFF for 10 seconds
+ TIME UNIT can be in seconds, minutes or hours
+ */
+
+#include <Switcher.h>       //Include Switcher library
+#define LED 13               // Define the led's pin
+bool LED_INIT = HIGH;        //We define HIGH as the INITIAL state of the led
+
+Switcher led(LED, LED_INIT);    // Define the Switcher led function led(LED pin, pin initial state: HIGH or LOW )
+
+void setup()
+{
+  pinMode(LED,OUTPUT);            // pinMode should be defined during setup
+  digitalWrite(LED,LED_INIT);     // digitalWrite with the initial state for the pin should be defined during the setup
+
+}
+
+void loop()
+{
+    led.Period(5,10,0);       // Led will stay ON for 5 second, OFF for 10 seconds     
+}   
+//  led.Period(5,10,1);        // We could use minutes: Led will stay on for 5 minute, off for 10 minutes 
+//  led.Period(5,10,2);        // We could use hours: Led will stay on for 5 hour, off for 10 hours 
---
+++ examples/Timer/Timer.ino
@@ -0,0 +1,32 @@
+/*
+ This code will turn ON a LED attached to pin 13 when a condition is meet
+ It will stay ON for 5 seconds
+ 
+ Timer() requires initialization: Start() should be called to turn ON the LED
+ Timer() should be called as ofter as possible ( like every loop )
+ 
+ */
+
+#include <Switcher.h>          //Include Switcher library
+#define LED 13                  //Define the led's pin
+bool LED_INIT = HIGH;           //We define HIGH as the INITIAL state of the led
+
+Switcher led(LED, LED_INIT);   // Define the Switcher led function led( LED pin , initial state: HIGH or LOW )
+int condition = 0;              //Supposed variable condition on which we turn ON the led
+
+void setup()
+{
+  pinMode(LED,OUTPUT);            // pinMode should be defined during setup
+  digitalWrite(LED,LED_INIT);     // digitalWrite with the initial state for the pin should be defined during the setup
+}
+
+void loop()
+{
+  led.Timer(5,0);               // Led will stay ON for 5 seconds, then it will turn OFF
+  if (condition = 0) {
+    led.Start();                // It will turn ON the led
+    condition = 1;
+  }
+}
+//  led.Timer(5,1);   // Led will stay ON for 5 minute
+//  led.Timer(5,2);   // Led will stay ON for 5 hour
