8e344dba08f497597392c08e44bf7bc4f9026b66
yyuri <oriol.goma@gmail.com>
2019-09-11 11:48:09 +0200

    Update StartandStop.ino

--- examples/StartandStop1/StartandStop.ino
+++ examples/StartandStop1/StartandStop.ino
@@ -1,12 +1,12 @@
 /*
- This code will blink an LED attached to pin 13 on and off. 
- It will stay on for 0.25 seconds.
- It will stay off for 1 second.
+ This code will turn ON and OFF a LED attached to pin 13 according to random conditions.
+ It will turn HIGH the LED pin when a random number condition is meet
+ It will turn LOW the LED pin when another random number condition is meet
  */
-#include <Switcher.h>   //Include Metro library
+#include <Switcher.h>   //Include Switcher library
 #define LED 13           // Define the led's pin
-bool LED_init = HIGH;    //We define HIGH as the INITIAL state of the led
-
+bool LED_init = LOW;    //We define LOW as the INITIAL state of the led
+int randomNum;
 Switcher led(LED, LED_init);
 
 
@@ -18,8 +18,11 @@ void setup()
 
 void loop()
 {
+randomnum = random(1,100);
+if ( randomNum > 91 ) {
 led.Start();   //If we call led.Start() it will turn LED pin 13 into LOW
-delay(300);
-led.Stop();    //If we call led.Stop() it will turn LED pin 13 into HIGH
-delay(300);
+}
+else if (randomNum < 10 ) {
+ led.Stop();    //If we call led.Stop() it will turn LED pin 13 into HIGH
+}
 }
